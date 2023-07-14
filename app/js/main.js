76261430d2d9c099a44beb8f0e3db3489e65a4f5
Татьяна <ltasya72@gmail.com>
2023-07-14 12:04:32 +0300

    Завершена testimonial

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,25 @@ $(function () {
     });
   })
 
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    slidesPerView: 1,
+    // spaceBetween: 20,
+  
+    navigation: {
+      nextEl: ".swiper-button-right",
+      prevEl: ".swiper-button-left",
+    },
+    // breakpoints: {
+    //   0: {
+    //     slidesPerView: 1,
+    //   },
+    //   768: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 20,
+    //   },
+    // },
+  });
 // const swiper = new Swiper('.swiper', {
 //     // Optional parameters
 //     direction: 'vertical',
