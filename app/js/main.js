419c41dfd7c768d6a038d4d12f1c5571e470469f
Татьяна <ltasya72@gmail.com>
2023-07-19 20:00:56 +0300

    переделан work-proc

--- app/js/main.js
+++ app/js/main.js
@@ -79,6 +79,20 @@ if ($(window).width() < 641) {
     $(".social").appendTo($(".nav"));
 }
 
+$(".hero__wrapp").on("click","a", function (event) {
+  //отменяем стандартную обработку нажатия по ссылке
+  event.preventDefault();
+
+  //забираем идентификатор бока с атрибута href
+  var id  = $(this).attr('href'),
+
+  //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+  
+  //анимируем переход на расстояние - top за 1500 мс
+  $('body,html').animate({scrollTop: top}, 1500);
+});
+
 // $(function () {
 //     $(".top__slider").slick({
 //         arrows: false,
